5d8dc9a9699787ee1f612f6fabb852c877c04a31
ramzi merad <ramzi07merad@gmail.com>
2024-12-03 10:38:12 +0000

    add Procfile

--- resources/views/me/index.blade.php
+++ resources/views/me/index.blade.php
@@ -8,7 +8,8 @@
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container box_1620">
                     <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
+                    
+                    {{-- <a class="navbar-brand logo_h" href="#html"><img src="img/logo.png" alt="Merad Ramzi"></a> --}}
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -69,6 +70,7 @@
                                     <li><a href="{{ $info->fb }}"><i class="fa fa-facebook"></i></a></li>
                                     <li><a href="{{ $info->ig }}"><i class="fa fa-instagram"></i></a></li>
                                     <li><a href="{{ $info->linkedin }}"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="https://github.com/ramzy07merad"><i class="fa fa-github"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -88,27 +90,21 @@
                 <div class="col-lg-6">
                     <div class="welcome_text">
                         <h4>About Myself</h4>
-                        <p>{{ $info->abt_me }}</p>
+                        <p>Driven by a strong professional ethics and the ability to work independently. Solid skills in organization and collaboration within a team.</p>
                         <div class="row">
-                            <div class="col-md-4">
-                                <div class="wel_item">
-                                    <i class="lnr lnr-database"></i>
-                                    <h4>$2.5M</h4>
-                                    <p>Total Donation</p>
-                                </div>
-                            </div>
+                            
                             <div class="col-md-4">
                                 <div class="wel_item">
                                     <i class="lnr lnr-book"></i>
-                                    <h4>1465</h4>
+                                    <h4>9</h4>
                                     <p>Total Projects</p>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="wel_item">
                                     <i class="lnr lnr-users"></i>
-                                    <h4>3965</h4>
-                                    <p>Total Volunteers</p>
+                                    <h4> +5 years </h4>
+                                    <p> experiences</p>
                                 </div>
                             </div>
                         </div>
@@ -150,10 +146,10 @@
                                 </div>
                             </div>
                             <div class="skill_item">
-                                <h4>React-Bootstrap  <span class="counter">75</span>%</h4>
+                                <h4>React-Bootstrap  <span class="counter">85</span>%</h4>
                                 <div class="progress_br">
                                     <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                 </div>
                             </div>
@@ -269,28 +265,28 @@
         <div class="container">
             <div class="main_title">
                 <h2>offerings to my clients</h2>
-                <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $.17 each.</p>
+                <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price.</p>
             </div>
             <div class="feature_inner row">
                 <div class="col-lg-4 col-md-6">
                     <div class="feature_item">
                         <i class="fi fi-rr-site-browser"></i>
                         <h4>WEB-SITE</h4>
-                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $17 each.</p>
+                        <p>as Full Stack Developer,I specialize in building and maintaining both the front-end and back-end of web applications Ihave a deep understanding of the full development process, from designing intuitive user interfaces to creating scalable server-side solutions.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="feature_item">
                         <i class="fi fi-ts-calendar-arrow-down"></i>
                         <h4>data scientist</h4>
-                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $17 each.</p>
+                        <p>As a Data Scientist, I specialize in analyzing and interpreting data to help organizations make informed,I leverage a combination of statistical methods, machine learning models, and data visualization techniques to uncover patterns, trends, and insights from data.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="feature_item">
                         <i class="fi fi-ts-devices"></i>
                         <h4>IT ENGINEER</h4>
-                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $17 each.</p>
+                        <p>As an IT Engineer, I specialize in managing and optimizing an organization’s technology infrastructure to ensure efficient . My role involves working with hardware, software, networks, and systems to maintain seamless technology functionality .</p>
                     </div>
                 </div>
             </div>
@@ -305,7 +301,7 @@
                 <h2>Our Latest Featured Projects</h2>
                 <p>Who are in extremely love with eco friendly system.</p>
             </div>
-            <div class="isotope_fillter">
+ {{--            <div class="isotope_fillter">
                 <ul class="gallery_filter list">
                     <li class="active" data-filter="*"><a href="#">All</a></li>
                     <li data-filter=".brand"><a href="#">Vector</a></li>
@@ -313,51 +309,51 @@
                     <li data-filter=".creative"><a href="#">UI/UX</a></li>
                     <li data-filter=".design"><a href="#">Printing</a></li>
                 </ul>
-            </div>
+            </div> --}}
         </div>
         <div class="container">
             <div class="gallery_f_inner row imageGallery1">
                 <div class="col-lg-4 col-md-4 col-sm-6 brand manipul design print">
                     <div class="h_gallery_item">
                         <div class="g_img_item">
-                            <img class="img-fluid" src="img/gallery/project-1.jpg" alt="">
-                            <a class="light" href="img/gallery/project-1.jpg"><img src="img/gallery/icon.png" alt=""></a>
+                            <img class="img-fluid" src="{{ asset('assets/machine.png') }}" alt="">
+                            <a class="light" href="{{ asset('assets/machine.png') }}"><img src="{{ asset('assets/machine.png') }}" alt=""></a>
                         </div>
                         <div class="g_item_text">
-                            <h4>3D Helmet Design</h4>
-                            <p>Client Project</p>
+                            <h4>Machine Learning</h4>
+                            <p>Over 20 Project</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 brand manipul creative">
                     <div class="h_gallery_item">
                         <div class="g_img_item">
-                            <img class="img-fluid" src="{{ asset('assets/img/gallery/project-2.jpg') }}" alt="">
-                            <a class="light" href="img/gallery/project-2.jpg"><img src="img/gallery/icon.png" alt=""></a>
+                            <img class="img-fluid" src="{{ asset('assets/ecommerce.png') }}" alt="">
+                            <a class="light" href={{asset("assets/ecommerce.png")}}><img src={{asset("assets/ecommerce.png")}} alt=""></a>
                         </div>
                         <div class="g_item_text">
-                            <h4>2D Vinyl Design</h4>
-                            <p>Client Project</p>
+                            <h4>FreeIt</h4>
+                            <p>E-commerce Dynamique web-site</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 manipul creative design print">
                     <div class="h_gallery_item">
                         <div class="g_img_item">
-                            <img class="img-fluid" src="img/gallery/project-3.jpg" alt="">
-                            <a class="light" href="img/gallery/project-3.jpg"><img src="img/gallery/icon.png" alt=""></a>
+                            <img class="img-fluid" src="{{ asset('assets/fennecbooks.jpeg') }}"  style= height:165px width="300px" alt="">
+                            <a class="light" href="{{ asset('assets/fennecbooks.jpeg') }}"><img src="{{ asset('assets/fennecbooks.jpeg') }}" alt=""></a>
                         </div>
                         <div class="g_item_text">
-                            <h4>Creative Poster Design</h4>
-                            <p>Client Project</p>
+                            <h4>Fennec Books</h4>
+                            <p>E-commerce Dynamique web-site for library </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 brand creative print">
+             {{--    <div class="col-lg-4 col-md-4 col-sm-6 brand creative print">
                     <div class="h_gallery_item">
                         <div class="g_img_item">
-                            <img class="img-fluid" src="img/gallery/project-4.jpg" alt="">
-                            <a class="light" href="img/gallery/project-4.jpg"><img src="img/gallery/icon.png" alt=""></a>
+                            <img class="img-fluid" src="{{ asset('assets/machine.png') }}" alt="">
+                            <a class="light" href="{{ asset('assets/machine.png') }}"><img src="{{ asset('assets/machine.png') }}" alt=""></a>
                         </div>
                         <div class="g_item_text">
                             <h4>Embosed Logo Design</h4>
@@ -388,17 +384,17 @@
                             <p>Client Project</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
-            <div class="more_btn">
+      {{--       <div class="more_btn">
                 <a class="main_btn" href="#">Load More Items</a>
-            </div>
+            </div> --}}
         </div>
     </section>
     <!--================End Home Gallery Area =================-->
     
     <!--================Testimonials Area =================-->
-    <section class="testimonials_area p_120">
+   {{--  <section class="testimonials_area p_120">
         <div class="container">
             <div class="main_title">
                 <h2>Testimonials</h2>
@@ -442,7 +438,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
     <!--================End Testimonials Area =================-->
     
     <!--================Footer Area =================-->
@@ -456,7 +452,7 @@
                         </div>
                         <p>Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills,</p>
                         <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright All rights reserved </P>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </aside>
                 </div>
@@ -466,7 +462,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                             <h3>Newsletter</h3>
                         </div>
                         <p>Stay updated with our latest trends</p>
-                        <div id="mc_embed_signup">
+                        {{-- <div id="mc_embed_signup">
                             <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
                                 <div class="input-group d-flex flex-row">
                                     <input name="EMAIL" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
@@ -474,7 +470,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                                 </div>				
                                 <div class="mt-10 info"></div>
                             </form>
-                        </div>
+                        </div> --}}
                     </aside>
                 </div>
                 <div class="col-lg-2">
@@ -484,10 +480,10 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                         </div>
                         <p>Let us be social</p>
                         <ul class="list">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                            <li><a href="{{ $info->fb }}"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="{{ $info->ig }}"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="{{ $info->linkedin }}"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="https://github.com/ramzy07merad"><i class="fa fa-github"></i></a></li>
                         </ul>
                     </aside>
                 </div>
